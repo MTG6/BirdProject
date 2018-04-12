02ab5a5b1f0dde15cb274dbfd7b37beb1d3c52e4
Matthew Gallagher <matthewgallagher@matthews-mbp.lan>
2018-04-11 19:52:58 -0500

    Create initial app

---
+++ Bird_App.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from app import app
+from develop.eda_functions import top5_bird
+from develop.data_input import *
+import pandas as pd
+
+
+#Homepage
+@app.route('/')
+def index():
+	res = 'Bird Paradise'
+	
+	#Code
+	df = read_data('Oregon')
+	top5 = top5_bird(df, year = 2010)
+	
+	#return variables
+	return render_template("landing.html" , output1=res , output2=top5)
+
+
+
+if __name__ == "__main__":
+	app.run(debug=True, host="0.0.0.0")
